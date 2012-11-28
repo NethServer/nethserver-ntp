6b98e64fe94072d68f1f6d8cce5226908962a933
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2012-11-28 18:15:41 +0100

    Translation: refactor english and italian translation files #523

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_DateTime.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_DateTime.php
@@ -1,20 +1,16 @@
-<?php
-/**
- */
+<?php 
 
-$L['DateTime_Title'] = 'Date and time';
-$L['DateTime_Description'] = 'Set system date and time' ;
-$L['date_label'] = 'Date';
-$L['time_label'] = 'Hours and minutes';
-$L['timezone_label'] = 'Timezone';
-$L['server_label'] = 'NTP Server';
-$L['status_enabled_label'] = 'Network synchronization';
-$L['status_disabled_label'] = 'Manual settings';
-$L['Date and time configuration'] = 'Set system date and time';
-$L["NTP server problem:\n\${0}"] = "An error occured with NTP server:\n\"\${0}\"";
-
-$L['time-manual-update_success'] = 'New system date and time is set.';
-$L['time-auto-update_success'] = 'New system date and time is set using network synchronization.';
-
-$L['valid_platform,ntp-server,ntp-server,1'] = 'NTP service is not available: ${reason}';
-$L['DateTime_Tags'] = 'hour minute timezone date time ntp';
+$L['Date and time configuration'] = 'Set system date and time'; 
+$L['DateTime_Description'] = 'Set system date and time'; 
+$L['DateTime_Tags'] = 'hour minute timezone date time ntp'; 
+$L['DateTime_Title'] = 'Date and time'; 
+$L["NTP server problem:\n${0}"] = "An error occured with NTP server:\n\"${0}\""; 
+$L['date_label'] = 'Date'; 
+$L['server_label'] = 'NTP Server'; 
+$L['status_disabled_label'] = 'Manual settings'; 
+$L['status_enabled_label'] = 'Network synchronization'; 
+$L['time-auto-update_success'] = 'New system date and time is set using network synchronization.'; 
+$L['time-manual-update_success'] = 'New system date and time is set.'; 
+$L['time_label'] = 'Hours and minutes'; 
+$L['timezone_label'] = 'Timezone'; 
+$L['valid_platform,ntp-server,ntp-server,1'] = 'NTP service is not available: ${reason}'; 
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_DateTime.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_DateTime.php
@@ -1,23 +1,16 @@
-<?php
-/**
- */
+<?php 
 
+$L['Date and time configuration'] = 'Impostazione data e ora'; 
+$L['DateTime_Description'] = 'Imposta la data e l\'ora del sistema'; 
+$L['DateTime_Tags'] = 'ora minuto fuso orario fusorario data ntp'; 
 $L['DateTime_Title'] = 'Data e ora';
-$L['DateTime_Description'] = 'Imposta la data e l\'ora del sistema' ;
-$L['date_label'] = 'Data';
-$L['time_label'] = 'Ora e minuti';
-$L['timezone_label'] = 'Fuso orario';
-$L['server_label'] = 'Server NTP';
-$L['status_enabled_label'] = 'Sincronizza tramite rete';
-$L['status_disabled_label'] = 'Imposta manualmente';
-$L['Date and time configuration'] = 'Impostazione data e ora';
-$L["NTP server problem:\n\${0}"] = "Problema con il sever NTP:\n\"\${0}\"";
-
-$L['time-auto-update_success'] = 'L\'ora di sistema è configurata per la sincronizzazione via rete.';
-$L['time-manual-update_success'] = 'L\'ora di sistema è configurata.';
-$L['time-auto-update_success'] = 'Errore durante la configurazione dell\'ora di sistema.';
-$L['time-manual-update_success'] = 'Errore durante la configurazione dell\'ora di sistema.';
-
-$L['DateTime_Tags'] = 'ora minuto fuso orario fusorario data ntp';
-
-$L['valid_platform,ntp-server,ntp-server,1'] = 'Servizio NTP non disponibile: ${reason}';
+$L["NTP server problem:\n${0}"] = "Si è verificato un errore con il server NTP:\n\"${0}\""; 
+$L['date_label'] = 'Data'; 
+$L['server_label'] = 'Server NTP'; 
+$L['status_disabled_label'] = 'Imposta manualmente'; 
+$L['status_enabled_label'] = 'Sincronizza tramite rete'; 
+$L['time-auto-update_success'] = 'Errore durante la configurazione dell\'ora di sistema.'; 
+$L['time-manual-update_success'] = 'Errore durante la configurazione dell\'ora di sistema.'; 
+$L['time_label'] = 'Ora e minuti'; 
+$L['timezone_label'] = 'Fuso orario'; 
+$L['valid_platform,ntp-server,ntp-server,1'] = 'Servizio NTP non disponibile: ${reason}'; 
