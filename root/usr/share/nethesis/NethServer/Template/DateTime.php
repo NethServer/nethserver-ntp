8d4313a48d34e4af4f3bd709be737dd1fafdf9fd
Davide Principi <davide.principi@nethesis.it>
2013-05-30 15:06:49 +0200

    DateTime UI module template: time controls enabled only if ntpd is not running; fixed translations. Refs #1988

--- root/usr/share/nethesis/NethServer/Template/DateTime.php
+++ root/usr/share/nethesis/NethServer/Template/DateTime.php
@@ -1,14 +1,16 @@
 <?php
 
-echo $view->header()->setAttribute('template', 'Date and time configuration');
+echo $view->header()->setAttribute('template', $T('DateTime_header'));
 
 echo $view->selector('timezone', $view::SELECTOR_DROPDOWN);
 
-echo $view->dateInput('date');
-echo $view->textInput('time');
+echo $view->fieldsetSwitch('status', 'disabled')
+    ->setAttribute('label', $T('Status_disabled_label'))
+    ->insert($view->dateInput('date'))
+    ->insert($view->textInput('time'));
 
-echo $view->fieldsetSwitch('status', 'disabled');
 echo $view->fieldsetSwitch('status', 'enabled')
+    ->setAttribute('label', $T('Status_enabled_label'))
     ->insert($view->textInput('server'));
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
